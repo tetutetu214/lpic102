20666896f8bb3be16a16f7e37a1dfa1c8d46b301
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-10-16 11:55:15 +0900

    1155

--- 109.sh
+++ 109.sh
@@ -46,6 +46,8 @@ nslookup
 getent
 host
 dig
+#getentコマンド：名前解決の検索
+/etc/nsswitch.confの設定に従い検索するコマンド
 
 #Netcat:TCP/UDP通信を手軽に行うことができるツール（接続の待ち受けが出来るので簡易サーバとしても利用可）
 nc -l 指定したアドレス、ポート番号で接続の待ち受け
@@ -68,23 +70,33 @@ wifi    無線
 
 #nmcliコマンド＝NetworkManagerを管理するためのコマンドラインインタフェース（CLI）
 nmcli オブジェクト[コマンド]
+#NMの状態
       general status #NetworkManagerの状態を表示する
               hostname #ホスト名を設定できる
+#ネットワーク
       networking connectivity #ネットワークの接続状態を表示できる
         full    ネットアクセス可能
 				portal  ログインしないとネットアクセスできない
 				limited ネットワークに接続しているが、ネットアクセスできない
 				none    接続していない
 				unknown 見つからない
+#Wifi(ちょい名前が特殊)
 			radio wifi  on/off  wifiの有効化無効化（接続させるまではdevice）
+#接続状況
 			connection  modify ID(接続IDの設定ファイルを変更する)
                   up/down (コネはupdown)
+									show(接続状況を表示)
+#デバイスの状況
 			device      show(IPアドレスなどの詳細情報を表示)
+			            status（デバイスの状態）
                   delete（ソフトウェアデバイスを削除）
                   modify インターフェイス(インターフェイスの設定ファイルを変更する)
 									wifi connect(wifiにアクセスポイント接続のための作成コマンド)
 									connect/disconnect(ネットワークデバイスの接続切断)
+									list(Wifiアクセスポイントの表示)
+									monitor(デバイスの監視)
 #nslookup:DNSへ問い合わせ
+ホスト名をIPアドレスに変更できる
 
 #digコマンド：DNSへ直接問い合わせ（DNSに関するデバッギングツール）
   DNSサーバから詳細な情報
@@ -105,16 +117,12 @@ host -v 詳細な出力
 host -t mx yahoo.co.jp
 
 #ウェルノウンポート
-20 FTP
-23 TELNET
-25 SMTP
+20 FTP    データ
+21 FTP    制御
+23 TELNET 遠隔操作
+25 SMTP   
 
 #ipconfig：Windows環境
-#ifconfig(interface configuration)：Linux環境のネットワークインターフェースの情報の表示や設定
-IP     （IPアドレス）
-subnet （サブネット）
-up/down（インターフェース有効無効）
-MTU     (転送量)
 
 #ipコマンド（ipコマンドなので、経路＝viaが使用されている）
 ##デフォルトルートを削除する
@@ -124,21 +132,30 @@ ip route del default via 192.168.122.1
 ip -s link show デバイス :-sオプションでバイト数などの統計情報を表示できる
 
 ip addr        IPv4,IPv6アドレス
+  ip addr show(ip a sh と省略できる)
 ip link        ネットワークデバイス（インターフェイスのこと）
+  ip link set eth0 up （インタフェイスの有効化）
+	ip link show(ip a sh のように省略できない！！)
 ip neighbor sh ARPキャッシュの内容を確認
 ip route       ルーティングテーブル
 
-#netstat:有効なネットワーク接続や開いているソケットの情報等を表示
+#ifconfig(interface configuration)：Linux環境のネットワークインターフェースの情報の表示や設定
+有線LAN・無線LANなどのインターフェイスにIPアドレス、サブネットマスクなどの設定ができる
+ipコマンドに移行しようとするも難航している
+
+IP     （IPアドレス）
+subnet （サブネット）
+up/down（インターフェース有効無効）
+MTU     (転送量)
+
+#netstat:有効なネットワーク接続や開いているソケットの情報等を表示（古い）
+TCP/UDPを対象にして統計情報を示す
 netstat    有効なネットワーク表示、ソケット情報表示
-netstat -r ルーティングテーブ表示
-netstat -i ネットワークインターフェースの統計情報
+netstat -r ルーティングテーブ表示                (ルーティング)
+netstat -i ネットワークインターフェースの統計情報(とうけiiii)
 netstat -n DNS無しの表示
 
-#whois:ドメインの管理者所有者情報を表示
-
-#dig:DNSサーバへ問い合わせ
-
-#ssコマンド:netstatの後継コマンド
+#ssコマンド:netstatの後継コマンド（新しい）
 ・ifconfig      インターフェイスの設定や表示
 ・routeコマンド ルーティングテーブルの設定
 ・arp
@@ -150,8 +167,15 @@ ss -n サービス名の名前解決をしない
 ss -t TCPソケット表示
 ss -u UDPソケット表示
 
+#whois:ドメインの管理者所有者情報を表示
+
+#dig:DNSサーバへ問い合わせ
+
+#hostnameコマンド
+ホストネームを一時的に変更
+
 #設定ファイル
-/etc/hostname          #Debianホスト名
+/etc/hostname          #Debianホスト名（恒久的に変更）
 /etc/sysconfig/network #RedHatホスト名・ネットワーク機能・デフォげ
 /etc/hosts             #IPアドレスとホスト名                       113.78.123.10  ping-t.com
 /etc/nsswitch.conf     #名前解決の順序                             hosts:         filess dns
