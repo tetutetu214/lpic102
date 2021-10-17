8695b82afe51ab5de80a69d85a9a54b2f8557c4d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-10-17 17:14:30 +0900

    1715

--- EX_102_1.sh
+++ EX_102_1.sh
@@ -1,6 +1,9 @@
 #idコマンド
   ユーザのUID/GIDがわかる
 
+#groupsコマンド
+  ユーザの所属するグループを表示
+
 #usermod/groupmod
   既存のアカウントの設定変更
 
@@ -36,6 +39,16 @@ ntpd あくまでサーバ側（/etc/ntp.conf）
 #xinetd
 log_type ログの出力先を記述（出力先は絶対パス）
 
+各サービスの個別設定
+(例)
+/etc/xinetd.d/myservice
+service myservice
+{
+	disable = yes
+}
+上記の場合だとサービスは起動しない
+disable=無効 に yes=する
+
 ホスト名
 /etc/hostname          だいたい同じ文字数
 /etc/sysconfig/network
@@ -68,6 +81,8 @@ IP
 #ntpqコマンド
 -p 一覧表示
 -i 対話モード（不思議だけど対話モードがある）
+#ntpdateコマンド
+あくまでNTPサーバで動作しているデーモン（ntpd）であり、このデーモンが時間を取得、反映させるわけではない。
 
 #CUPS(印刷)
 CUPS
@@ -86,11 +101,28 @@ cat/etc/cups/printers.conf プリンター自体のハード系
 /.profile
 /.bashrc
 
+#ログインシェルとは異なる対話型シェルの起動の際
+/etc/bash.bashrc
+/etc/bashrc
+~/.bashrc
+
+全ユーザ
+/etc/bash.bashrc Debian系
+/etc/bashrc      RedHat系
+
 #nmcliコマンド
 device monitor デバイスを監視
 device wifi list         アクセスポイントの表示
             connect SSID 接続デバイス作成・有効化
             rescan       再検索
+networking connectivity
+            full   
+            portal 
+            limited 
+            none    
+            unknown 
+#これと間違えていた！！
+connection up/down
 
 #crontabコマンド
 -e 編集
@@ -111,3 +143,73 @@ noglob(No Global) 正規表現NG
 ignoreeof         Ctrl+ログアウトNG
 noclobber         ＞＜演算子NG
 noexec            実行NG
+
+#Chrony(クローニー)
+NTPサーバ/クライアントに代替するシステム
+
+#ポート
+20 FTP   ファイル転送
+21 FTP
+22 SSH    リモート操作（暗号化）
+23 TELNET リモート操作（暗号なし）
+
+#digコマンド
+→DNSに設定している値を直接みる（中を見ても値で表記されている）
+#whoisコマンド
+→DNSに登録している会社の窓口（ドメイン登録者を日本語で）とかを確認できる
+
+#xdmの設定ファイル
+/etc/X11/xdm/
+
+#エラー出力
+~/.xsession-errors(ホームに隠しファイルといて出力される)
+
+#groupdelコマンド グループアカウント自体の削除
+
+#ルーティング
+コマンドの出力詳細がわかる（2つは同じ）
+route
+netstat -r
+
+コマンドの出力のみ（簡易的）
+ip route show
+
+#印刷ジョブ
+lprm -PPrinterB 500(500というジョブ番号は数値のみで表示)
+
+#ulimit
+-a all
+-c core
+-f file
+-u userprocess
+-v virtual memory
+
+#タイムゾーン
+/usr/share/zoneinfo のタイムゾーン情報を
+/etc/localtime      にコピー（ただしバイナリ）
+/etc/timezone       上記をテキストファイルにして利用できるように設定
+
+#journalctlコマンド
+-D 指定したディレクトリのlogを表示
+-b boot
+-o output ログの出力を指定
+
+#netstat  ネットワーク接続、ソケット情報（TCPなど）
+#ifconfig インターフェイスの状態(部分)
+
+#findコマンドの書式について
+find / -perm +6000   SUID or  SGID
+find / -perm -6000   SUID and SGID
+
+SSHサーバ（sshd）
+  /etc/ssh/sshd_config
+SSHクライアント（ssh）
+  /etc/ssh/ssh_config
+
+#aliasコマンドの設定
+alias rm='rm -i'(バッククオートNG)
+
+#localeコマンド
+locale    今の設定
+locale -a 設定できる一覧
+
