6c8e231219f8b760d33c5387ee60f84924e077d1
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-10-20 13:28:28 +0900

    1328

---
+++ command.sh
@@ -0,0 +1,44 @@
+/etc/locale.conf
+user -g root user1
+揮発性ストレージ
+timedatectl
+-i 
+/etc/cron.weekly/
+/etc/shadow
+su - user1
+
+unalias -a
+-r
+set -o vi
+-x
+set -o noexec
+
+4964
+4968
+systemd-journaldデーモン
+表示不可能なログも全て出力
+-a
+表示可能なログを全て出力する
+-l
+--full
+リアルタイム出力
+-f
+
+
+4918
+/usr/share/zoneinfo/
+/etc/localtime
+/etc/timezone
+
+4604
+nmap リモートホストのポート確認
+lsof ローカルホストのポート確認
+fuser ポート確認&&killコマンド
+
+4779
+プリンタの共有設定ファイル
+/etc/cups/printers.conf
+4807
+ntpdate ntpから時間の取得
+
+4643
